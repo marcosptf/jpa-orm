c0cf59825cd25cfd9755f7ff3274282a23276f65
marcosptf <marcosptf@yahoo.com.br>
2015-06-09 20:56:44 -0300

    add jpa code

---
+++ src/mysql/entity.sql
@@ -0,0 +1,341 @@
+
+git rm 01. Hackers Crackers e Phreakers.pdf
+git commit -m "add new ebook structure"
+
+git rm 0201775948 by Luke Hohmann 2003.chm
+git commit -m "add new ebook structure"
+git rm 111563562-Games-Independentes.pdf
+git rm 12-Honeypots.pdf
+git commit -m "add new ebook structure"
+git rm 1791_Curso De Hebraico_Port.pdf
+git commit -m "add new ebook structure"
+git rm 1capitulo_fugacampo14.pdf
+git commit -m "add new ebook structure"
+git rm 2011-Head First HTML5 Programming.pdf
+git commit -m "add new ebook structure"
+git rm 83-312-1-PB.pdf
+git commit -m "add new ebook structure"
+git rm 912_ebookmercadorevelado.pdf
+git commit -m "add new ebook structure"
+git rm 9781849517386_Chapter_01.pdf
+git commit -m "add new ebook structure"
+git rm A Bibliography of the Personal Software.pdf
+git commit -m "add new ebook structure"
+git rm AJ1.pdf
+git commit -m "add new ebook structure"
+git rm AJ2.pdf
+git commit -m "add new ebook structure"
+git rm AJ4.pdf
+git commit -m "add new ebook structure"
+git rm AJ5.pdf
+git commit -m "add new ebook structure"
+git rm A_Certificacao_ZCE-@EdgarSandi.pdf
+git commit -m "add new ebook structure"
+git rm Addison.Wesley.Design.Patterns.Explained.A.New.Perspective.On.Object.Oriented.Design.Jul.2001.ISBN.0201715945.pdf
+git commit -m "add new ebook structure"
+git rm Addison.Wesley.Design.Patterns.In.Java.2nd.Edition.Apr.2006.ISBN.0321333020.pdf
+git commit -m "add new ebook structure"
+git rm Addison.Wesley.Patterns.Of.Enterprise.Application.Architecture.Nov.2002.ISBN.0321127420.chm
+git commit -m "add new ebook structure"
+git rm Addison.Wesley.Refactoring.Ruby.Edition.Oct.2009.ISBN.0321603508.pdf
+git commit -m "add new ebook structure"
+git rm Addison.Wesley.The.Rails.4.Way.Jun.2014.ISBN.0321944275.pdf
+git commit -m "add new ebook structure"
+git rm Apress.Beginning.POJOs.Mar.2006.ISBN.1590595963.pdf
+git commit -m "add new ebook structure"
+git rm Apress.Pro.Java.6.3D.Game.Development.Java.3D.JOGL.JInput.And.JOAL.APIs.Apr.2007.ISBN.1590598172.pdf
+git commit -m "add new ebook structure"
+git rm Assembly Language Step by Step 1992.pdf
+git commit -m "add new ebook structure"
+git rm BACKTRACK_CUDA_v2.0.pdf
+git commit -m "add new ebook structure"
+git rm BPMN-11-01-03 Version 2.0.pdf
+git commit -m "add new ebook structure"
+git rm Behat-sfLive12.pdf
+git commit -m "add new ebook structure"
+git rm Beyond Software Architecture.pdf
+git commit -m "add new ebook structure"
+git rm Bible - American Standard Version.pdf
+git commit -m "add new ebook structure"
+git rm CEACAA00418KNK.PDF
+git commit -m "add new ebook structure"
+git rm Cocos2d.for.iPhone.0.99.Beginners.Guide.Dec.2010.pdf
+git commit -m "add new ebook structure"
+git rm Como Se Dar Bem Com As Mulheres www.dgemg.info.pdf
+git commit -m "add new ebook structure"
+git rm DICIONRIODEACORDESPARATECLADO.pdf
+git commit -m "add new ebook structure"
+git rm Delacorte.The.Code.Book.Mar.2002.ISBN.0385729138.pdf
+git commit -m "add new ebook structure"
+git rm Design-Patterns.pdf
+git commit -m "add new ebook structure"
+git rm DesignPatterns.pdf
+git commit -m "add new ebook structure"
+git rm Developing Modern Mobile Web Apps.pdf
+git commit -m "add new ebook structure"
+git rm DiscoverMeteor-Templates-Sample.pdf
+git commit -m "add new ebook structure"
+git rm Dragonball_Z_RPG_-_Anime_Adventure_Game_-_Core_Rules.pdf
+git commit -m "add new ebook structure"
+git rm El-arte-de-la-intrusion.pdf
+git commit -m "add new ebook structure"
+git rm Ementas-AD_.pdf
+git commit -m "add new ebook structure"
+git rm Empresas Estrangeiras Regulares.pdf
+git commit -m "add new ebook structure"
+git rm Eric Evans 2003 - Domain-Driven Design - Tackling Complexity in the Heart of Software.pdf
+git commit -m "add new ebook structure"
+git rm Flash.Game.Development.by.Example.pdf
+git commit -m "add new ebook structure"
+git rm Frameworks_PHP-@EdgarSandi.pdf
+git commit -m "add new ebook structure"
+git rm Fuga do Campo 14 - Blaine Harden.pdf
+git commit -m "add new ebook structure"
+git rm Getting Started with RFID.pdf
+git commit -m "add new ebook structure"
+git rm Getting Started.pdf
+git commit -m "add new ebook structure"
+git rm Guia_ef_englishtown_phrasal_verbs.pdf
+git commit -m "add new ebook structure"
+git rm HTML5Games.pdf
+git commit -m "add new ebook structure"
+git rm HTML5GamesExcample.pdf
+git commit -m "add new ebook structure"
+git rm Hakin9_On_Deman_03_2012_Teasers.pdf
+git commit -m "add new ebook structure"
+git rm HiloJavaScript.pdf
+git commit -m "add new ebook structure"
+git rm Honeypots.pdf
+git commit -m "add new ebook structure"
+git rm Identificando CPUs.mht
+git commit -m "add new ebook structure"
+git rm Introducing.HTML5.Game.Development.pdf
+git commit -m "add new ebook structure"
+git rm LFS-BOOK.pdf
+git commit -m "add new ebook structure"
+git rm LM_104_CE.pdf
+git commit -m "add new ebook structure"
+git rm LM_105_CE.pdf
+git commit -m "add new ebook structure"
+git rm LM_115_70_76_07_davinci.pdf
+git commit -m "add new ebook structure"
+git rm Manning.AspectJ.In.Action.Enterprise.AOP.With.Spring.Applications.2nd.Edition.Sep.2009.ISBN.1933988053.pdf
+git commit -m "add new ebook structure"
+git rm Manning.Spring.In.Action.2nd.Edition.Aug.2007.ISBN.1933988134.pdf
+git commit -m "add new ebook structure"
+git rm Manning_-_Secrets_of_the_JavaScript_Ninja_MEAP__2009_.pdf
+git commit -m "add new ebook structure"
+git rm No.Starch.Black.Hat.Python.Dec.2014.ISBN.1593275900.pdf
+git commit -m "add new ebook structure"
+git rm Nothing_to_Envy_Ordinary_Lives_in_North_Korea.epub
+git commit -m "add new ebook structure"
+git rm OReilly.IOS.Dec.2014.ISBN.1491908696.pdf
+git commit -m "add new ebook structure"
+git rm ObjectJavaScript.pdf
+git commit -m "add new ebook structure"
+git rm Packt.Service.Oriented.Architecture.With.Java.Using.SOA.And.Web.Services.To.Build.Powerful.Java.Applications.Jun.2008.ISBN.1847193218.pdf                                                                                                             
+git commit -m "add new ebook structure"
+git rm Packt.jQuery.Game.Development.Essentials.1849695067.pdf
+git commit -m "add new ebook structure"
+git rm Pro HTML5 Programming.pdf
+git commit -m "add new ebook structure"
+git rm Programando em shell-script.pdf
+git commit -m "add new ebook structure"
+git rm PythonBrochure_20140327_15-43-51RZ102-DL.pdf
+git commit -m "add new ebook structure"
+git rm "SIT-PA-0007-Aplica\303\247\303\265es Marketing_v3-manual-claro-security.pdf"
+git commit -m "add new ebook structure"
+git rm "SIT-PA-0007-Aplica\303\247\303\265es Marketing_v3.pdf"
+git commit -m "add new ebook structure"
+git rm Seguranca_em_PHP-@EdgarSandi.pdf
+git commit -m "add new ebook structure"
+git rm Sidney Simoes e Silva Filho - Artigo.pdf
+git commit -m "add new ebook structure"
+git rm Silex.pdf
+git commit -m "add new ebook structure"
+git rm Springer.Guide.To.Assembly.Language.A.Concise.Introduction.Mar.2011.ISBN.0857292706.pdf
+git commit -m "add new ebook structure"
+git rm Springer.Guide.To.Assembly.Language.Programming.In.Linux.Jul.2005.ISBN.0387258973.pdf
+git rm Syngress.Black.Oct.2004.ISBN.193226681X.pdf
+git commit -m "add new ebook structure"
+git rm Syngress.Physical.And.Logical.Security.Convergence.Powered.By.Enterprise.Security.Management.May.2007.ISBN.1597491225.pdf                                                                                                                             
+git commit -m "add new ebook structure"
+git rm Team Software Process.pdf
+git commit -m "add new ebook structure"
+git rm Teste de Software - Aula 3.pdf
+git commit -m "add new ebook structure"
+git rm The Personal Software Process Traduzido.doc
+git commit -m "add new ebook structure"
+git rm The Personal Software Process.doc
+git commit -m "add new ebook structure"
+git rm The Personal Software Process.pdf
+git commit -m "add new ebook structure"
+git rm The Personal.pdf
+git commit -m "add new ebook structure"
+git rm The Team Software Process Traduzido.doc
+git commit -m "add new ebook structure"
+git rm The Team Software Process.doc
+git commit -m "add new ebook structure"
+git rm TheHackersHandbook.pdf
+git commit -m "add new ebook structure"
+git rm The_C_Programming_Language_Ritchie___Kernighan_Second_Edition.ebook-pdf.pdf
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly2.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly3.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly4.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly5.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly6.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly7.mht
+git commit -m "add new ebook structure"
+git rm Tutorial de Linguagem Assembly8.mht
+git commit -m "add new ebook structure"
+git rm "Uma Vida com Prop\303\263sitos.pdf"
+git commit -m "add new ebook structure"
+git rm Unit 1 Master 1 sat.pptx
+git commit -m "add new ebook structure"
+git rm Unit 2 Master 1 sat.pptx
+git commit -m "add new ebook structure"
+git rm Unit 2_2 Master 1 sat.pptx
+git commit -m "add new ebook structure"
+git rm Unit 2_3_showtime Master 1 sat.pptx
+git commit -m "add new ebook structure"
+git rm "Vers\303\243o traduzida The Personal Software Process.doc"
+git commit -m "add new ebook structure"
+git rm "Vers\303\243o traduzida de The Team Software Process.doc"
+git commit -m "add new ebook structure"
+git rm WebMatrix.pdf
+git commit -m "add new ebook structure"
+git rm Wiley.Testing.Python.Jul.2014.ISBN.1118901223.pdf
+git commit -m "add new ebook structure"
+git rm Wrox.C.Sharp.5.0.Programmers.Reference.Apr.2014.ISBN.1118847288.pdf
+git commit -m "add new ebook structure"
+git rm ZendFramework-2.1.0-manual-en.pdf
+git commit -m "add new ebook structure"
+git rm [E]Pro Java6 3D Game Development Java3D, JOGL, JInput and JOAL, APIs.pdf
+git commit -m "add new ebook structure"
+git rm [gift] DesignPatterns.pdf
+git commit -m "add new ebook structure"
+git rm android game development - thrilling experience of 2d and 3d games.pdf
+git commit -m "add new ebook structure"
+git rm apprendrelequebec-accessible.pdf
+git commit -m "add new ebook structure"
+git rm backtrack-hd-install.pdf
+git commit -m "add new ebook structure"
+git rm backtrack4-guide-tutorial.pdf
+git commit -m "add new ebook structure"
+git rm bh-us-03-spitzner.pdf
+git commit -m "add new ebook structure"
+git rm bible-king-james-version.pdf
+git commit -m "add new ebook structure"
+git rm danielMastral-FilhosDoFogo-1.pdf
+git commit -m "add new ebook structure"
+git rm danielMastral-FilhosDoFogo-2.pdf
+git commit -m "add new ebook structure"
+git rm desenvolvimentodejogoscomjavascriptehtml1-110710155103-phpapp02.pdf
+git commit -m "add new ebook structure"
+git rm e-mail.pdf
+git commit -m "add new ebook structure"
+git rm esabBancoDeDados.pdf
+git commit -m "add new ebook structure"
+git rm esabRedes.pdf
+git commit -m "add new ebook structure"
+git rm exemplosDeProgramacaoComMSDebug.mht
+git commit -m "add new ebook structure"
+git rm exemplosDeProgramacaoComTasm.mht
+git commit -m "add new ebook structure"
+git rm "fundamentosSistemasInforma\303\247\303\243o.pdf"
+git commit -m "add new ebook structure"
+git rm head_first_design_patterns.pdf
+git commit -m "add new ebook structure"
+git rm honeypot_honeynet.pdf
+git commit -m "add new ebook structure"
+git rm iPhone Game Development.pdf
+git commit -m "add new ebook structure"
+git rm js-secrets.pdf
+git commit -m "add new ebook structure"
+git rm makinggames.pdf
+git commit -m "add new ebook structure"
+git rm material-engenharia-de-software-faculdade-esab.pdf
+git commit -m "add new ebook structure"
+git rm metodologiaAnaliseSistemas.pdf
+git commit -m "add new ebook structure"
+git rm monografia_sobre_design_patterns.pdf
+git commit -m "add new ebook structure"
+git rm mysqleobigdata-htitecnologia-07ago2013-130807161435-phpapp01.pdf
+git commit -m "add new ebook structure"
+git rm nasmdoc.pdf
+git commit -m "add new ebook structure"
+git rm nbso-ssi2003-mchnets.pdf
+git commit -m "add new ebook structure"
+git rm nosBastidoresDaNintendo.pdf
+git commit -m "add new ebook structure"
+git rm open_proxy_honeypots.pdf
+git commit -m "add new ebook structure"
+git rm oracle20150304-dl.pdf
+git commit -m "add new ebook structure"
+git rm php_development_best_practices.pdf
+git commit -m "add new ebook structure"
+git rm progit.en.pdf
+git commit -m "add new ebook structure"
+git rm restful_web_services_cookbook.pdf
+git commit -m "add new ebook structure"
+git rm segredos_javascripter_ninja.pdf
+git commit -m "add new ebook structure"
+git rm sistemasOperacionaisESAB.pdf
+git commit -m "add new ebook structure"
+git rm teclado-cursocompleto-comotocarteclado-rafaelharduim-110105175725-phpapp01.pdf
+git commit -m "add new ebook structure"
+git rm test-driven-development-teste-e-design-no-mundo-real_gelso-luiz-da-rocha-junior.pdf
+git commit -m "add new ebook structure"
+git rm vimbook-OPL.pdf
+git commit -m "add new ebook structure"
+git rm whitepaper.pdf
+git commit -m "add new ebook structure"
+git status
+git push origin master
+
+
+
+
+
+
+
+
+git add assembler/
+git commit -m "add new ebook structure"
+git add devgames/
+git commit -m "add new ebook structure"
+git add fedora/
+git commit -m "add new ebook structure"
+git add hack/
+git commit -m "add new ebook structure"
+git add java/
+git commit -m "add new ebook structure"
+git add literature/
+git commit -m "add new ebook structure"
+git add php/
+git commit -m "add new ebook structure"
+git add python/
+git commit -m "add new ebook structure"
+git add ruby/
+git commit -m "add new ebook structure"
+git add softwareEngine/
+git commit -m "add new ebook structure"
+
+
+
+git rm XNA 4.0 Game Development by Example_ Beginner's Guide - Kurt Jaegers.opf
+git commit -m "add new ebook structure"
+git rm XNA 4.0 Game Development by Example_ Beginner's Guide - Kurt Jaegers.pdf
+git commit -m "add new ebook structure"
+git push origin master
+git status
+
+
