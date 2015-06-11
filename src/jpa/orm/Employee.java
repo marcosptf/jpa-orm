1ea0587b60e0dc0dcf08de9e9e294e4e03772f3d
marcosptf <marcosptf@yahoo.com.br>
2015-06-11 12:43:44 -0300

    add new feactures to jpa code

--- src/jpa/orm/Employee.java
+++ src/jpa/orm/Employee.java
@@ -8,7 +8,9 @@ package jpa.orm;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 /**
  *
@@ -20,6 +22,9 @@ public class Employee {
     @Id
     private int id;
     private String name;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastAccessTime;
     
     public Employee(){ }
     
@@ -43,4 +48,16 @@ public class Employee {
         this.name = name;
     }
     
+    public Date getLastAccessTime(){
+        return lastAccessTime;
+    }
+    
+    public void setLastAccessTime(Date lastAccessTime){
+        this.lastAccessTime = lastAccessTime;
+    }
+    
+    @Override
+    public String toString(){
+        return "Employee " + getId() + " name: " + getName() + " lastAccessTime" + getLastAccessTime();
+    }
 }
